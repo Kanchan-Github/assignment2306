2111f776feaa0cb3bde271946207b45ff40af880
saurav <sauree987@gmail.com>
2020-09-24 08:36:44 +1000

    Serializing the object to the file

--- itech2306/utility/ObjectIO.java
+++ itech2306/utility/ObjectIO.java
@@ -11,24 +11,38 @@ public class ObjectIO implements Serializable{
 	 
 	    public static void main(String args[]) {
 	 
-	        ObjectIO objectIO = new ObjectIO();
+	       ObjectIO objectIO = new ObjectIO();
 	 
 	        Customer customer = new Customer(1,"Derek",2218);
 	        objectIO.WriteObjectToFile(customer);
+	        
+	      /*  try {
+	        	writeToFile(customer);
+	        } catch (IOException e) {
+	        	System.out.println(e.getMessage());
+	        }
 	    }
+	    */
 	 
-	    public void WriteObjectToFile(Object serObj) {
+	    public void WriteObjectToFile(Object customer) {
 	 
 	        try {
 	 
 	            FileOutputStream fileOut = new FileOutputStream(filepath);
 	            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
-	            objectOut.writeObject(serObj);
+	            objectOut.writeObject(customer);
 	            objectOut.close();
 	            System.out.println("The Object is succesfully written to a file");
 	 
 	        } catch (Exception ex) {
 	            ex.printStackTrace();
+	        }
+	        public static void writeFile(Person p) throws IOException{
+	        	ObjectOutputStregam objectOutputStream = new ObjectOutputStream(new FileOutputStream("Customer.bin"));
+	        	objectOutputStream.writeObject(customer);
+	        }
+	        public static void readFile() {
+	        	ObjectInputStream objectInputStream = new ObjectInputStream(new FileInput)
 	        }
 	    }
 	}
